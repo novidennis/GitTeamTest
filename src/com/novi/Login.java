b40a5d056e6e9c5b7594da3039d16271a51e3feb
Dennis Haverhals <dennis.haverhals@noviuniversity.com>
2020-01-29 10:05:29 +0100

    login controller

---
+++ src/com/novi/Login.java
@@ -0,0 +1,7 @@
+package com.novi;
+
+public class Login {
+    public void login() {
+        System.out.println("Login Dennis");
+    }
+}
--- src/com/novi/Main.java
+++ src/com/novi/Main.java
@@ -3,6 +3,8 @@ package com.novi;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dennis");
+        System.out.println("Main Dennis");
+        Login loginController = new Login();
+        loginController.login();
     }
 }
