74932a7977849da5bfe03920e576d785ee025e1d
Dennis Haverhals <dennis.haverhals@noviuniversity.com>
2020-01-29 10:38:53 +0100

    main commit - conflict test

--- src/com/novi/Main.java
+++ src/com/novi/Main.java
@@ -3,6 +3,7 @@ package com.novi;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dennis");
+        System.out.println("Hello Main");
+        System.out.println("What is your name?");
     }
 }
